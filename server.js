5127373fd72f357c7ad3922b16282e5d7fb2de9e
hanjunt <429256563@qq.com>
2021-01-18 09:32:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const express = require('express');
+
+const app = express();
+
 app.use(express.static('./dist/website'));
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: './dist/website'}
-  );
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/website/'}),
+);
 
 app.listen(process.env.PORT || 8080);
